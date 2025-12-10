5fc7d6b382081a8abd21d5ac17f40fa2a726b5a1
CCS-zozs <togyousyou@gmail.com>
2025-12-10 09:11:09 +0800

    bug fix publication_date

--- myproject/bookapp/models.py
+++ myproject/bookapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=20)
     author = models.CharField(max_length=10)
-    published_date = models.DateField()
+    publication_date = models.DateField()
 
     def __str__(self):
         return self.title
