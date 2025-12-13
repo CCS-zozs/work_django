b75bbc9fc63dac9d1406657786821d05c15c015a
CCS-zozs <togyousyou@gmail.com>
2025-12-13 15:22:10 +0800

    add Validation

--- myproject/bookapp/forms.py
+++ myproject/bookapp/forms.py
@@ -16,3 +16,29 @@ class BookForm(forms.ModelForm):
             'publication_date': forms.DateInput(attrs={'type': 'date'}),
         }
         
+        error_messages = {
+            'title': {
+                'max_length': 'タイトルが長すぎます（最大20文字）。',
+                'required': 'タイトルは必須項目です。',
+            },
+            'author': {
+                'max_length': '著者名が長すぎます（最大10文字）。',
+                'required': '著者名は必須項目です。',
+            },
+        }
+        
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if '盗作' in title:
+            raise forms.ValidationError('タイトルに「盗作」を含めることはできません。')
+        return title
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get('title')
+        author = cleaned_data.get('author')
+        
+        if title == author:
+            raise forms.ValidationError('タイトルと著者名は同じにできません。')
+        return
+        
