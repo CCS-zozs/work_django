1155dc3073fd3a822fd3ec8c464f223d34a4bd9c
CCS-zozs <togyousyou@gmail.com>
2025-12-19 17:08:36 +0800

    add createView

--- myproject/todoapp/urls.py
+++ myproject/todoapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     # 'list'というURLにアクセスすると、views.pyのTodoListViewクラスが呼び出される
     # name='todo_list'は、このURLパターンに名前を付けている（テンプレートなどで参照可能）
     path('list/', views.TodoListView.as_view(), name='todo_list'),
+    path('<int:pk>/', views.TodoDetailView.as_view(), name='todo_detail'),
+    path('new/', views.TodoCreateView.as_view(), name='todo_create'),
 ]
--- myproject/todoapp/views.py
+++ myproject/todoapp/views.py
@@ -1,7 +1,8 @@
 from . import models # 現在のアプリケーションのモデルをインポート
 
 # Djangoのクラスベースビューをインポート
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 # ============================================================
 # ToDo一覧表示用のビュー：継承元はListView
@@ -13,3 +14,14 @@ class TodoListView(ListView):
     template_name = 'todoapp/todo_list.html'
     # テンプレートで使用するオブジェクトリストの名前を指定
     context_object_name = 'todos'
+    
+class TodoDetailView(DetailView):
+    model = models.Todo
+    template_name = 'todoapp/todo_detail.html'
+    context_object_name = 'todo'
+    
+class TodoCreateView(CreateView):
+    model = models.Todo
+    template_name = 'todoapp/todo_create.html'
+    fields = ['title', 'memo', 'completed']
+    success_url = reverse_lazy('todo_list')
