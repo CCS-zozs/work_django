fafa5f190de8c3c6472d2a5e3541d048baeaa4f5
CCS-zozs <togyousyou@gmail.com>
2025-12-23 09:09:02 +0800

    add messages

--- myproject/todoapp/views.py
+++ myproject/todoapp/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 from django.utils.timezone import localtime
 from .forms import TodoForm
+from django.contrib.messages.views import SuccessMessageMixin
 
 # ============================================================
 # ToDo一覧表示用のビュー：継承元はListView
@@ -22,24 +23,27 @@ class TodoDetailView(DetailView):
     template_name = 'todoapp/todo_detail.html'
     context_object_name = 'todo'
     
-class TodoCreateView(CreateView):
+class TodoCreateView(SuccessMessageMixin, CreateView):
     model = models.Todo
     template_name = 'todoapp/todo_create.html'
     form_class = TodoForm
     success_url = reverse_lazy('todo_list')
-    
-class TodoUpdateView(UpdateView):
+    success_message = "Todo項目が作成されました。"
+
+class TodoUpdateView(SuccessMessageMixin, UpdateView):
     model = models.Todo
     template_name = 'todoapp/todo_update.html'
     form_class = TodoForm
     success_url = reverse_lazy('todo_list')
-    
+    success_message = "Todo項目が更新されました。"
+
     def form_valid(self, form):
         todo = form.save()
         print(f"Todo項目が更新されました: {todo.title} (更新日時: {localtime(todo.updated)})")
         return super().form_valid(form)
 
-class TodoDeleteView(DeleteView):
+class TodoDeleteView(SuccessMessageMixin, DeleteView):
     model = models.Todo
     template_name = 'todoapp/todo_confirm_delete.html'
     success_url = reverse_lazy('todo_list')
+    success_message = "Todo項目が削除されました。"
